e89e371a67b416c8f637520c5b75146fa0bbe0da
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2020-07-07 14:51:29 +0200

    PUBDEV-4007 add test against full output of the text representation

--- h2o-py/tests/testdir_algos/gbm/pyunit_4007_decision_tree_language_rules_printing.py
+++ h2o-py/tests/testdir_algos/gbm/pyunit_4007_decision_tree_language_rules_printing.py
@@ -1,9 +1,7 @@
 from __future__ import print_function
 from builtins import range
 import sys
-
 from h2o.tree import H2OTree
-
 sys.path.insert(1,"../../../")
 import h2o
 from tests import pyunit_utils, math
@@ -21,9 +19,7 @@ def decision_tree_language_rules_printing():
     gle[gle == 0] = None
     train['CAPSULE'] = train['CAPSULE'].asfactor()
     train.describe()
-    my_gbm = H2OGradientBoostingEstimator(ntrees=50,
-                                          learn_rate=0.1,
-                                          distribution="bernoulli", max_depth = 2)
+    my_gbm = H2OGradientBoostingEstimator(ntrees=50, learn_rate=0.1, distribution="bernoulli", max_depth = 2, seed = 12345)
     my_gbm.train(x=list(range(1, train.ncol)),
                  y="CAPSULE",
                  training_frame=train,
@@ -32,34 +28,55 @@ def decision_tree_language_rules_printing():
 
     print(" -- Tree predictions: -- ")
     print(first_tree.predictions)
+    
     print(" -- Language tree representation: -- ")
     assert first_tree.language_tree_representation is not None
     print(first_tree.language_tree_representation)
+    text_file = open("pyunit_4007_language_tree_representation_numerical_case.txt", "r")
+    expected_tree_representation = text_file.read()
+    text_file.close()
+    assert expected_tree_representation == first_tree.language_tree_representation
+    
     print(" -- Language path representation - root node: -- ")
     assert first_tree.language_path_representations[first_tree.root_node.id] is not None
     print(first_tree.language_path_representations[first_tree.root_node.id])
+    
     print(" -- Language path representation - node ", first_tree.predictions.index(first_tree.predictions[3]), " (with pv = ", first_tree.predictions[3], "): -- ")
     assert first_tree.language_path_representations[first_tree.predictions.index(first_tree.predictions[3])] is not None
     print(first_tree.language_path_representations[first_tree.predictions.index(first_tree.predictions[3])])
-    
+    text_file = open("pyunit_4007_language_path_representation_numerical_case.txt", "r")
+    expected_tree_representation = text_file.read()
+    text_file.close()
+    assert expected_tree_representation == first_tree.language_path_representations[first_tree.predictions.index(first_tree.predictions[3])]
     
     print(" ----- categorical case: -----")
     airlines_data = h2o.import_file(path=pyunit_utils.locate("smalldata/airlines/allyears2k_headers.zip"))
-    model = H2OGradientBoostingEstimator(ntrees = 3, max_depth = 2)
+    model = H2OGradientBoostingEstimator(ntrees = 3, max_depth = 2, seed = 12345)
     model.train(x=["Origin", "Distance"], y="IsDepDelayed", training_frame=airlines_data)
     tree = H2OTree(model = model, tree_number = 0, tree_class = "NO")
 
     print(" -- Tree predictions: -- ")
     print(tree.predictions)
+    
     print(" -- Language tree representation: -- ")
     assert tree.language_tree_representation is not None
     print(tree.language_tree_representation)
+    text_file = open("pyunit_4007_language_tree_representation_categorical_case.txt", "r")
+    expected_tree_representation = text_file.read()
+    text_file.close()
+    assert expected_tree_representation == tree.language_tree_representation
+
     print(" -- Language path representation - root node: -- ")
     assert tree.language_path_representations[tree.root_node.id] is not None
     print(tree.language_path_representations[tree.root_node.id])
+    
     print(" -- Language path representation - node ", tree.predictions.index(tree.predictions[3]), " (with pv = ", tree.predictions[3], "): -- ")
     assert tree.language_path_representations[tree.predictions.index(tree.predictions[3])] is not None
     print(tree.language_path_representations[tree.predictions.index(tree.predictions[3])])
+    text_file = open("pyunit_4007_language_path_representation_categorical_case.txt", "r")
+    expected_tree_representation = text_file.read()
+    text_file.close()
+    assert expected_tree_representation == tree.language_path_representations[tree.predictions.index(tree.predictions[3])]
     
 if __name__ == "__main__":
     pyunit_utils.standalone_test(decision_tree_language_rules_printing)
